615556565bd148df007dad1d552a7f48a61b03a3
Alex Hawkins <alexhawkins.me@gmail.com>
2014-12-05 00:52:31 -0800

    Implement Post View

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,3 +1,19 @@
-app.controller('PostsCtrl', function($scope){
+app.controller('PostsCtrl', function($scope) {
+  $scope.posts = [];
+  $scope.post = {
+    url: 'http://',
+    title: ''
+  };
 
+  $scope.deletePost = function(index) {
+    $scope.posts.splice(index, 1);
+  };
+
+  $scope.submitPost = function() {
+    $scope.posts.push($scope.post);
+    $scope.post = {
+      url: 'http://',
+      title: ''
+    };
+  };
 });
